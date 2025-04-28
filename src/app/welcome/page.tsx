d9d9eddc816067f66fbbcacb78511a19e6ac8470
ManuelPerezF <A01741957@tec.mx>
2025-04-28 08:23:22 -0600

    frontend sin acabar

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function Welcome() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+      <h1 className="text-3xl font-bold text-center text-black">Welcome!</h1>
+      <p className="text-lg text-gray-700 mt-4">¡Hola, [nombre del empleado]! Bienvenido al portal de SkyTech..</p>
+    </main>
+  );
+}
